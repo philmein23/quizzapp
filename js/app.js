a71c52cbfea570deeb5f5b41817dc5bb9f5f925d
Phillip Nguyen <phillip.d.nguyen23@gmail.com>
2015-10-12 23:32:25 -0700

    added final score

--- js/app.js
+++ js/app.js
@@ -33,18 +33,23 @@ $(document).ready(function() {
             correctAnswer: "Andrew Rayel"
         }
 
-    ]
+    ];
 
     var counter = 0;
     var correctGuess = 0;
 
 
 
+    $("a.close").click(function() {
+        $(".answer").fadeOut('slow');
+    });
+
+
     $('.start').click(function(event) {
         event.preventDefault();
         /* Act on the event */
         $('p').empty();
-         $('.answer').empty();
+        $('.answer').empty();
         counter = 0;
         currentQuestion();
 
@@ -56,32 +61,47 @@ $(document).ready(function() {
         /* Act on the event */
 
         compareAnswers();
+
         counter++
         $('p').empty();
 
         currentQuestion();
 
 
+
     });
 
 
     function currentQuestion() {
-        $('p').append(quiz[counter].question + "<br /><br />")
+
+        if (counter < 5) {
+            $('p').append(quiz[counter].question + " <br /><br />")
+
+        } else {
+            $('p').empty();
+            $('p').append("You scored a total of " + correctGuess + " correct answers on the quiz!");
+            correctGuess = 0;
+        }
+
 
         for (var i = 0; i < quiz[counter].answers.length; i++) {
-            $('p').append("<input type='radio' name='" + quiz[counter].answers[i] + "' value='" + quiz[counter].answers[i] + "' />" + "<span class='result'>" + quiz[counter].answers[i] + "</span><br />")
+            $('p').append("<input type='radio' name='" + quiz[counter].answers[i] + "' value='" + quiz[counter].answers[i] + "' />" + "<span class='result'>" + quiz[counter].answers[i] + "</span><br />");
+
+
         }
 
     }
 
     function compareAnswers() {
-        var answerChosen = $("input[type='radio']:checked").val()
+        var answerChosen = $("input[type='radio']:checked").val();
         if (answerChosen === quiz[counter].correctAnswer) {
-            $(".answer").append("<h3>You are correct!</h3>")
+            $(".answer").append("<h3>You are correct!</h3>").fadeIn('fast');
+            correctGuess++;
         } else {
-            $(".answer").append("<h3>You are wrong!</h3>")
+            $(".answer").append("<h3>You are wrong!</h3>").fadeIn('fast');
         }
     }
+
 
 
 });
